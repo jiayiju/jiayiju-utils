49950052da10c7938d23edbb52bef1bcbe6d0012
jiayiju <38799121@qq.com>
2019-10-14 08:52:20 +0800

    10.14

--- src/main/java/com/jiayiju/utils/StringUtil.java
+++ src/main/java/com/jiayiju/utils/StringUtil.java
@@ -67,6 +67,7 @@ public class StringUtil {
 	 * public static String strReplace() { String ass=""; ass.rep }
 	 */
 
+	
 	public static boolean isNumber(String src) {
 		String reg = "[0-9][0-9]*";
 		return src.matches(reg);
